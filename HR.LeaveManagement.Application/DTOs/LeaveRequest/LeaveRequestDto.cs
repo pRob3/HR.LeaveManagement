a111931fa81c613502fbf75ff95bba7812416e1d
pRob3 <bubblan@slaf.se>
2021-10-09 06:38:10 +0200

    Implemented Add

--- HR.LeaveManagement.Application/DTOs/LeaveRequest/LeaveRequestDto.cs
+++ HR.LeaveManagement.Application/DTOs/LeaveRequest/LeaveRequestDto.cs
@@ -16,7 +16,7 @@ namespace HR.LeaveManagement.Application.DTOs
         public int LeaveTypeId { get; set; }
         public DateTime DateRequested { get; set; }
         public string RequestComments { get; set; }
-        public DateTime DateActioned { get; set; }
+        public DateTime? DateActioned { get; set; }
         public bool? Approved { get; set; }
         public bool Cancelled { get; set; }
     }
