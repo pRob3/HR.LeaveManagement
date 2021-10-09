c69d880b2e15222d6a5eabd13f0e65532f10f3bd
pRob3 <bubblan@slaf.se>
2021-10-09 07:41:21 +0200

    Implemented Update and Delete, basic CRUD done.

---
+++ HR.LeaveManagement.Application/Features/LeaveAllocations/Requests/Commands/UpdateLeaveAllocationCommand.cs
@@ -0,0 +1,16 @@
+﻿using HR.LeaveManagement.Application.DTOs;
+using HR.LeaveManagement.Application.DTOs.LeaveAllocation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocations.Requests.Commands
+{
+    public class UpdateLeaveAllocationCommand : IRequest<Unit>
+    {
+        public LeaveAllocationDto LeaveAllocationDto { get; set; }
+    }
+}
